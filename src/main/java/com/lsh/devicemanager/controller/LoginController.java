2ad62884acfd5436c38da0a5936fb3a023119f30
ued123 <32861341+ued123@users.noreply.github.com>
2023-04-25 08:25:35 +0900

    Initial commit

---
+++ src/main/java/com/lsh/devicemanager/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.lsh.devicemanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(@RequestParam(value = "error", required = false) String error, Model model) {
+        if (error != null) {
+            model.addAttribute("errorMsg", "Invalid username or password");
+        }
+        return "login";
+    }
+}
