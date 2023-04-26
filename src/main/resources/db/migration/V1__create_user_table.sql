2356875550f1ca84b7a4c162c03b847f7e071aad
ued123 <32861341+ued123@users.noreply.github.com>
2023-04-27 07:55:26 +0900

    V1__create_user_table.sql: 기본 관리자 계정 추가, date타입을 timestamp 로 변경 (java Instant 객체와 호환되도록)

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,8 +1,19 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 CREATE TABLE users (
    user_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
    name varchar(255) NOT NULL,
    password varchar(255) NOT NULL,
    hash_type varchar(50) NOT NULL,
-   update_date date NOT NULL,
-   create_date date NOT NULL
+   update_date timestamp NOT NULL,
+   create_date timestamp NOT NULL
 );
+
+INSERT INTO users (name, password, hash_type, update_date, create_date)
+VALUES (
+           'admin',
+           encode(digest('qwer1234', 'sha256'), 'hex'),
+           'sha256',
+           current_date,
+           current_date
+       );
