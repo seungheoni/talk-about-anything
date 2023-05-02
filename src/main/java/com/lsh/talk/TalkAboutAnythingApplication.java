d6ca8324237e2eecb2d7767b434b1efe29c46182
ued123 <32861341+ued123@users.noreply.github.com>
2023-05-03 07:57:46 +0900

    redis와 spring session을 활용한 세션id 분산 저장 build.gradle: 분산저장을 위한 라이브러리 추가 application.yml: 세션id 저장 설정 docker-compose.yml: 분산저장을 위한 redis 환경 추가 TalkAboutAnythingApplication: redis에 세션id 저장 설정을 위한 자동 어노테이션 추가

--- src/main/java/com/lsh/talk/TalkAboutAnythingApplication.java
+++ src/main/java/com/lsh/talk/TalkAboutAnythingApplication.java
@@ -2,8 +2,10 @@ package com.lsh.talk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class TalkAboutAnythingApplication {
 
     public static void main(String[] args) {
