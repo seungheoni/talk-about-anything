bd77043e13ec948eab02da24c6cb77a7d12dc6a1
이승헌 <32861341+ued123@users.noreply.github.com>
2023-05-03 07:18:00 +0900

    [#3] 로그인 controller, security config 설정 (#6)
    * 로그인 유저 정보 로딩 변경, sha256 인코드 설정
    application.yml: SecurityConfig 로그 디버깅 설정
    SecurityConfig: 유저정보 로딩방식 dbms로 변경 , passwordEncoder 설정
    SHA256PasswordEncoder: 패스워드 해싱 및 16진수 인코딩 클래스
    UserDetailServiceImpl: 시큐리티를 활용한 유저이름을 통한 로딩
    
    * 사용안하는 코드 제거

---
+++ src/main/java/com/lsh/talk/config/coder/SHA256PasswordEncoder.java
@@ -0,0 +1,40 @@
+package com.lsh.talk.config.coder;
+
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA256PasswordEncoder implements PasswordEncoder {
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(rawPassword.toString().getBytes(StandardCharsets.UTF_8));
+            return bytesToHex(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Failed to hash password", e);
+        }
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        String hashedPassword = encode(rawPassword);
+        return hashedPassword.equals(encodedPassword);
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+}
