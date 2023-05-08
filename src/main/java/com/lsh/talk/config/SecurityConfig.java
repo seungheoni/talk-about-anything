897d61643cee1db5fafcbaf74a63838da535c103
이승헌 <32861341+ued123@users.noreply.github.com>
2023-05-09 06:34:00 +0900

    [#4] 로그인 페이지 화면 개발 및 메인화면 틀 잡기 (#9)
    * 로그인 페이지 템플릿 경로 적용, 스프링 시큐리티 css 권한 설정
    application.yml: 타입리프 엔진 경로 설정 및 static 경로 설정
    login.css: 로그인페이지 관련 외부 css 파일
    login.html: css 외부 파일로 변경
    SecurityConfig: css 파일경로 시큐리티 허용
    
    * devtool 적용, 로그인 페이지 로고 삽입, 이미지 리소스 시큐리티 권한 설정
    build.gradle: devtool 라이브러리 추가
    application.yml: devtool 활성화 옵션 추가
    login.html, login.css: 로그인 로고 및 css 추가
    logo.png: 로고 추가
    SecurityConfig: 스프링 시큐리티 설저
    
    * 타입리프 설정 변경, 메인화면 폼 작성
    application.yml: 타입리프 설정 변경, 인코딩 ,모드 설정, 템플릿 유무 확인등 상세 주석 확인
    login.css: 디렉토리 수정 login 디렉토리 생성
    login.html: 디렉토리 수정 login 디렉토리 생성
    LoginController: 가라데이터 main 화면 생성
    main.html: 메인화면 생성
    SecurityConfig: 메인화면 관련하여 인증과 리다이렉트 설정
    
    * 가라데이터 문구 수정
    
    ---------
    
    Co-authored-by: 이승헌 <skydrive860@icent.co.kr>

--- src/main/java/com/lsh/talk/config/SecurityConfig.java
+++ src/main/java/com/lsh/talk/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,13 +18,16 @@ public class SecurityConfig {
         http
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
+                                .requestMatchers("/static/css/**","/static/image/**").permitAll()
+                                .requestMatchers("/main").authenticated()
                                 .anyRequest().authenticated()
                 )
                 .formLogin(formLogin ->
-                        formLogin.loginPage("/login").permitAll()
+                        formLogin.loginPage("/login")
+                                .defaultSuccessUrl("/main", true)
+                                .permitAll()
                 )
-                .logout(logout ->
-                        logout.permitAll()
+                .logout(LogoutConfigurer::permitAll
                 );
         return http.build();
     }
--- src/main/java/com/lsh/talk/controller/LoginController.java
+++ src/main/java/com/lsh/talk/controller/LoginController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
+
 @Controller
 public class LoginController {
 
@@ -13,6 +15,14 @@ public class LoginController {
         if (error != null) {
             model.addAttribute("errorMsg", "Invalid username or password");
         }
-        return "login";
+        return "login/login";
+    }
+
+    @GetMapping("/main")
+    public String home(Model model) {
+        model.addAttribute("username", "승헌");
+        model.addAttribute("friends", Arrays.asList("Friend 1", "Friend 2", "Friend 3"));
+        model.addAttribute("chats", Arrays.asList("Chat 1", "Chat 2", "Chat 3"));
+        return "main/main";
     }
 }
