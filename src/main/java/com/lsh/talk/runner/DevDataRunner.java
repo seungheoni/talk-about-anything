2e72e19c5071be247bf285c51cd393cba951637f
ued123 <32861341+ued123@users.noreply.github.com>
2023-05-17 07:23:48 +0900

    DevDataRunner: 개발 모드일때 더미 데이터 추가

--- src/main/java/com/lsh/talk/runner/DevDataRunner.java
+++ src/main/java/com/lsh/talk/runner/DevDataRunner.java
@@ -1,5 +1,6 @@
 package com.lsh.talk.runner;
 
+import com.lsh.talk.config.coder.SHA256PasswordEncoder;
 import com.lsh.talk.domain.ChatFriend;
 import com.lsh.talk.domain.ChatRoom;
 import com.lsh.talk.domain.ChatRoomParticipant;
@@ -8,36 +9,32 @@ import com.lsh.talk.repository.ChatFriendRepository;
 import com.lsh.talk.repository.ChatRoomParticipantRepository;
 import com.lsh.talk.repository.ChatRoomRepository;
 import com.lsh.talk.repository.ChatUserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
-import java.util.UUID;
 
 @Component
 @Profile("dev")
+@RequiredArgsConstructor
 public class DevDataRunner implements ApplicationRunner {
 
     private final ChatUserRepository chatUserRepository;
     private final ChatRoomRepository chatRoomRepository;
     private final ChatFriendRepository chatFriendRepository;
     private final ChatRoomParticipantRepository chatRoomParticipantRepository;
-
-    public DevDataRunner(ChatUserRepository chatUserRepository, ChatRoomRepository chatRoomRepository,
-                         ChatFriendRepository chatFriendRepository, ChatRoomParticipantRepository chatRoomParticipantRepository) {
-        this.chatUserRepository = chatUserRepository;
-        this.chatRoomRepository = chatRoomRepository;
-        this.chatFriendRepository = chatFriendRepository;
-        this.chatRoomParticipantRepository = chatRoomParticipantRepository;
-    }
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
         ChatUser seongheon = new ChatUser();
         seongheon.setName("seongheon");
-        seongheon.setPassword("password");
+        seongheon.setPassword(passwordEncoder.encode("qwer1234"));
         seongheon.setHashType("SHA-256");
         seongheon.setUpdateDate(Instant.now());
         seongheon.setCreateDate(Instant.now());
@@ -45,7 +42,7 @@ public class DevDataRunner implements ApplicationRunner {
 
         ChatUser friend1 = new ChatUser();
         friend1.setName("friend1");
-        friend1.setPassword("password");
+        friend1.setPassword(passwordEncoder.encode("qwer1234"));
         friend1.setHashType("SHA-256");
         friend1.setUpdateDate(Instant.now());
         friend1.setCreateDate(Instant.now());
@@ -53,27 +50,21 @@ public class DevDataRunner implements ApplicationRunner {
 
         ChatUser friend2 = new ChatUser();
         friend2.setName("friend2");
-        friend2.setPassword("password");
+        friend2.setPassword(passwordEncoder.encode("qwer1234"));
         friend2.setHashType("SHA-256");
         friend2.setUpdateDate(Instant.now());
         friend2.setCreateDate(Instant.now());
         chatUserRepository.save(friend2);
 
-        ChatUser seongheonRes = chatUserRepository.findById(seongheon.getId()).orElse(null);
-        ChatUser friend1Res = chatUserRepository.findById(friend1.getId()).orElse(null);
-
         ChatFriend chatFriend1 = new ChatFriend();
-        chatFriend1.setChatUser(seongheonRes);
-        chatFriend1.setFriendChatUser(friend1Res);
+        chatFriend1.setChatUser(seongheon);
+        chatFriend1.setFriendChatUser(friend1);
         chatFriend1.setCreatedDate(Instant.now());
         chatFriendRepository.save(chatFriend1);
 
-        ChatUser seongheonRes2 = chatUserRepository.findById(seongheon.getId()).orElse(null);
-        ChatUser friend1Res2 = chatUserRepository.findById(friend1.getId()).orElse(null);
-
         ChatFriend chatFriend2 = new ChatFriend();
-        chatFriend2.setChatUser(seongheonRes2);
-        chatFriend2.setFriendChatUser(friend1Res2);
+        chatFriend2.setChatUser(seongheon);
+        chatFriend2.setFriendChatUser(friend2);
         chatFriend2.setCreatedDate(Instant.now());
         chatFriendRepository.save(chatFriend2);
 
@@ -89,17 +80,15 @@ public class DevDataRunner implements ApplicationRunner {
         room2.setCreatedChatUser(seongheon);
         chatRoomRepository.save(room2);
 
-        ChatRoom chatRoomRes = chatRoomRepository.findById(seongheon.getId()).orElse(null);
-
         ChatRoomParticipant participant1 = new ChatRoomParticipant();
-        participant1.setChatRoom(chatRoomRes);
-        participant1.setChatUser(seongheonRes);
+        participant1.setChatRoom(room1);
+        participant1.setChatUser(seongheon);
         participant1.setJoinedDate(Instant.now());
         chatRoomParticipantRepository.save(participant1);
 
         ChatRoomParticipant participant2 = new ChatRoomParticipant();
-        participant2.setChatRoom(chatRoomRes);
-        participant2.setChatUser(seongheonRes2);
+        participant2.setChatRoom(room2);
+        participant2.setChatUser(seongheon);
         participant2.setJoinedDate(Instant.now());
         chatRoomParticipantRepository.save(participant2);
     }
