246724a829081fc950f2fefc3bd25cd3e313fa2c
이승헌 <32861341+ued123@users.noreply.github.com>
2023-05-25 07:06:41 +0900

    [#30][#31] 친구추가하기 및 csrf 보호 추가 (#32)
    * 친구추가,채팅하기 버튼 추가
    
    * 친구 추가하기 모달창 생성
    
    * 친구추가하기 기능 및 csrf 토큰 적용
    ChatUserService, ChatUserServiceImpl: 친구 추가하기 서비스 코드
    DevDataRunner: 개발 더미데이터 생성
    main.css: 모달창 크기 수정
    main.html: csrf 토큰 적용
    main.js: csrf토큰 적용 및 친구 추가 요청시 전송
    UserController: 친구 추가하기 api 엔드포인트 정의

---
+++ src/main/java/com/lsh/talk/dto/request/FriendRequest.java
@@ -0,0 +1,12 @@
+package com.lsh.talk.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FriendRequest {
+
+    private String friendName;
+}
--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -1,18 +1,102 @@
+window.onload = function() {
+    // Friends 탭에 대한 이벤트 리스너 추가
+    document.querySelector('.tablinks[data-tab="Friends"]').addEventListener('click', function(event) {
+        openTab(event, 'Friends');
+    });
+
+    // Chats 탭에 대한 이벤트 리스너 추가
+    document.querySelector('.tablinks[data-tab="Chats"]').addEventListener('click', function(event) {
+        openTab(event, 'Chats');
+    });
+
+    var modal = document.getElementById("myModal");
+
+    var closeButton = document.getElementsByClassName("close")[0];
+
+    closeButton.onclick = function() {
+        var modal = document.getElementById("myModal");
+
+        modal.classList.remove("open");
+        modal.classList.add("close");
+
+        setTimeout(function() {
+            modal.style.display = "none";
+        }, 500);
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    let addFriendSubmit = document.getElementById('add-friend-submit');
+    let csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
+    let csrfHeader = document.querySelector('meta[name="_csrf_header"]').getAttribute('content');
+
+    addFriendSubmit.addEventListener('click', function(event){
+        event.preventDefault();
+        let friendName = document.getElementById('friend-name').value;
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "/api/v1/add-friend", true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.setRequestHeader(csrfHeader, csrfToken);
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 204) {
+                    // 성공적으로 친구가 추가되었을 때 실행할 코드
+                    alert('친구 추가에 성공 했습니다');
+                    // 추가된 친구를 화면에 업데이트하는 로직을 작성하세요
+                } else {
+                    // 친구 추가 실패 시 실행할 코드
+                    alert('친구 추가 실패');
+                }
+            }
+        };
+
+        xhr.send(JSON.stringify({friendName: friendName}));
+    });
+};
+
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-}
+        tabcontent[i].style.display = "none";
+    }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-}
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
+
+    var addFriendButton = document.getElementById('add-friend-btn');
+    var startChatButton = document.getElementById('start-chat-btn');
+    if (tabName === 'Friends') {
+        addFriendButton.style.display = 'inline-block';
+        startChatButton.style.display = 'none';
+    } else if (tabName === 'Chats') {
+        addFriendButton.style.display = 'none';
+        startChatButton.style.display = 'inline-block';
+    } else {
+        addFriendButton.style.display = 'none';
+        startChatButton.style.display = 'none';
+    }
 }
 
 function openChatRoom(element) {
     var roomId = element.getAttribute('data-room-id');
     window.location.href = '/chatRoom/' + roomId;
+}
+
+function addFriendOpenModal() {
+
+    var modal = document.getElementById("myModal");
+
+    modal.classList.remove("close");
+    modal.classList.add("open");
+
+    modal.style.display = "block";
 }
