3dd4899cc3717830272f38287eca14dfa9a454d4
이승헌 <32861341+ued123@users.noreply.github.com>
2023-05-31 08:00:08 +0900

    [#36] 채팅방 생성하기 모달창 및 생성버튼 개발 (#37)

--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -57,6 +57,21 @@ window.onload = function() {
 
         xhr.send(JSON.stringify({friendName: friendName}));
     });
+
+
+
+    // 새로운 채팅방 생성하기
+    document.querySelector('#start-chat-btn').addEventListener('click', startChat);
+
+    var chatCloseButton = document.getElementById("chatModal").getElementsByClassName("close")[0];
+    chatCloseButton.onclick = function() {
+        var modal = document.getElementById("chatModal");
+        modal.classList.remove("open");
+        modal.classList.add("close");
+        setTimeout(function() {
+            modal.style.display = "none";
+        }, 500);
+    }
 };
 
 function openTab(evt, tabName) {
@@ -123,6 +138,51 @@ function updateFriendList() {
                 friendListElement.appendChild(li);
             }
 
+        }
+    };
+
+    xhr.send();
+}
+
+function startChat() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/v1/friends', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            var friends = JSON.parse(xhr.responseText);
+            var friendListElement = document.getElementById('start-chat-form');
+
+            // 기존 친구 목록을 비우기
+            while (friendListElement.firstChild) {
+                friendListElement.removeChild(friendListElement.firstChild);
+            }
+
+            // 새 친구 목록을 추가하기
+            for (var i = 0; i < friends.length; i++) {
+                var div = document.createElement('div');
+
+                var input = document.createElement('input');
+                input.type = "checkbox";
+                input.id = friends[i].friendName;
+                input.value = friends[i].friendName;
+
+                var label = document.createElement('label');
+                label.htmlFor = friends[i].friendName;
+                label.textContent = friends[i].friendName;
+
+                div.appendChild(input);
+                div.appendChild(label);
+                friendListElement.appendChild(div);
+            }
+
+            var modal = document.getElementById("chatModal");
+
+            modal.classList.remove("close");
+            modal.classList.add("open");
+
+            modal.style.display = "block";
         }
     };
 
